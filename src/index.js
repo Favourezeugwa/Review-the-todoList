641de51961bb2c2ac933b3a22fd04fc7e1f37298
Favourezeugwa <favourezeugwa@gmail.com>
2022-06-06 15:17:11 +0100

    Add todolist src directory

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import _ from 'lodash';
+import './style.css';
+
+import listItems from './modules/createlist.js';
+import { list, SaveItem } from './modules/class.js';
+
+const form = document.querySelector('#add-form');
+const toDoList = document.querySelector('.to-do-list');
+const clearCompleted = document.querySelector('#clear-btn');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const itemSaved = new SaveItem();
+  itemSaved.addNew();
+  form.reset();
+});
+
+for (let i = 1; i <= list.length; i += 1) {
+  list.forEach((x) => {
+    if (x.index === i) {
+      listItems(x);
+    }
+  });
+}
+
+// clear all completed fields
+clearCompleted.addEventListener('click', () => {
+  const newlist = list.filter((element) => element.completed === false);
+  list.forEach((element) => {
+    if (!newlist.includes(element)) {
+      list.splice(list.indexOf(element), 1);
+    }
+  });
+  // to update the index
+  let i = 1;
+  list.forEach((element) => {
+    element.index = i;
+    i += 1;
+  });
+  toDoList.innerHTML = '';
+  for (let i = 1; i <= list.length; i += 1) {
+    list.forEach((listItem) => {
+      if (listItem.index === i) {
+        listItems(listItem);
+      }
+    });
+  }
+  localStorage.setItem('list', JSON.stringify(list));
+});
---
+++ src/modules/createlist.js
@@ -0,0 +1,84 @@
+import { list, SaveItem } from './class.js';
+import check from './checkbox.js';
+
+const toDoList = document.querySelector('.to-do-list');
+
+const listItems = (listItem, id) => {
+  const li = document.createElement('li');
+  toDoList.appendChild(li);
+  const div = document.createElement('div');
+  div.classList.add('list-container');
+  li.appendChild(div);
+
+  const checkbox = document.createElement('input');
+  checkbox.setAttribute('type', 'checkbox');
+  checkbox.classList.add('check-list');
+  div.appendChild(checkbox);
+
+  const text = document.createElement('input');
+  text.setAttribute('type', 'text');
+  text.setAttribute('placeholder', listItem.description);
+  text.setAttribute('disabled', '');
+  div.appendChild(text);
+
+  const button = document.createElement('button');
+  button.setAttribute('type', 'button');
+  button.innerHTML = `
+    <i class="fa fa-ellipsis-vertical"></i>`;
+
+  button.addEventListener('click', (event) => {
+    const itemParent = event.target.parentNode.parentNode;
+    itemParent.querySelector('.fa-trash-can').parentNode.style.display = 'block';
+    button.style.display = 'none';
+    li.style.background = '#f7cee9';
+    // to edit text
+    text.disabled = false;
+    text.focus();
+  });
+  li.appendChild(button);
+
+  // delete button
+  const deleteButton = document.createElement('button');
+  deleteButton.setAttribute('type', 'button');
+  deleteButton.innerHTML = `
+    <i class='fa-solid fa-trash-can'></i>`;
+  deleteButton.style.display = 'none';
+
+  deleteButton.id = id;
+  deleteButton.addEventListener('click', (event) => {
+    const deleteItem = new SaveItem();
+    deleteItem.removeItem(id);
+  });
+  li.appendChild(deleteButton);
+
+  // edit text Item
+  text.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+      text.placeholder = text.value;
+      button.style.display = 'flex';
+      deleteButton.style.display = 'none';
+      text.disabled = true;
+      li.style.background = 'none';
+      const Item = new SaveItem();
+      Item.editItem(id, text.value);
+    }
+  });
+
+  // checkbox event listener
+  if (listItem.completed) {
+    checkbox.checked = true;
+    text.style.textDecoration = 'line-through';
+  }
+  let x = {};
+  list.forEach((element) => {
+    if (element === listItem) {
+      x = element;
+    }
+  });
+  checkbox.addEventListener('click', (event) => {
+    check(event, x, text);
+    localStorage.setItem('list', JSON.stringify(list));
+  });
+};
+
+export default listItems;
